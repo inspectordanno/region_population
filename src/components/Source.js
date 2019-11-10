425095c42dd95e14e8813cf122f46999a3718236
Dan Spector <danno2423@gmail.com>
2019-11-10 00:59:03 -0500

    Update Source.js

--- src/components/Source.js
+++ src/components/Source.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Source = () => (
   <div className="source">
     <div>
-    Source: United Nations. 2014. World Urbanization Prospects 2009, 2014 Revision. 
+    Source: United Nations. 2014.  
+    </div>
+    <div>
+    World Urbanization Prospects 2009, 2014 Revision.
     </div>
     <div>
     http://esa.un.org/unpd/wup/highlights/wup2014-highlights.pdf
